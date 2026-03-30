4f90472199c9dd64e2f0489f001a1aa95188a33c
susana-zhou1 <susana.zhou1@nhs.net>
2026-03-30 16:57:52 +0100

    ENG-991 update dist file

--- dist/index.cjs
+++ dist/index.cjs
@@ -19975,8 +19975,8 @@ async function run() {
     });
     core.setOutput("status-code", result.statusCode.toString());
     core.setOutput("response-body", result.body);
-    const body = JSON.parse(result.body);
     if (result.statusCode >= 200 && result.statusCode < 300) {
+      const body = JSON.parse(result.body);
       if (body.pipelinePasses === "false") {
         core.setFailed(
           `${LOG_STYLE.bold}${LOG_STYLE.red}Policy check failed:${LOG_STYLE.reset} 
